f33dc9cfc57eebedf5b759abf4f0360a4e2f7160
Jose Alvarez Loredo <48250416+EnriqueCS@users.noreply.github.com>
2024-05-21 22:14:32 +0000

    This should fix it

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const TodoModel = require("./Models/Todo.js");
+
+const TodoSchema = new mongoose.Schema({
+    date: String,
+    task: String,
+    color: String,
+    address: String
+})
+const TodoModel = mongoose.model("Todo", TodoSchema)
 
 const app = express();
 const PORT = 3001;
