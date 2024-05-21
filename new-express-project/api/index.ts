03b66b7f9cba7640cea63afb0f3c01f92fde95a1
Jose Alvarez Loredo <48250416+EnriqueCS@users.noreply.github.com>
2024-05-21 23:48:00 +0000

    Deploy

--- new-express-project/api/index.ts
+++ new-express-project/api/index.ts
@@ -5,11 +5,16 @@ const serverless = require('serverless-http');
 const app = express();
 const port = process.env.PORT || 3001; // Use the environment's port if available or default to 3001
 
-// Middleware setup
+const allowedOrigins = ['https://buggy-tracker-rio6.vercel.app'];
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 app.use(express.json());
